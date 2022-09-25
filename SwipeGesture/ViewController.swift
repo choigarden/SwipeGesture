3226b34df2bfa424feb2afe7074d740ccdb67b75
jeongwon <choigarden@nate.com>
2022-09-25 20:38:51 +0900

    18-3 아웃렛 변수 추가하기

--- SwipeGesture/ViewController.swift
+++ SwipeGesture/ViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imgViewUp: UIImageView!
+    
+    @IBOutlet var imgViewDown: UIImageView!
+    
+    @IBOutlet var imgViewLeft: UIImageView!
+    
+    @IBOutlet var imgViewRight: UIImageView!
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
